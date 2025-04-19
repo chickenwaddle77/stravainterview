de510edd2c8098c56cb3c822e7ed2285786c6575
Steven Lin <yuchenglin0705@gmail.com>
2025-04-19 00:00:10 -0400

    Initial Commit

---
+++ Strava Interview Prep2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strava Interview Prep2
+//
+//  Created by Steven Lin on 2025-04-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Strava Interview Prep2Tests/Strava_Interview_Prep2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Strava_Interview_Prep2Tests.swift
+//  Strava Interview Prep2Tests
+//
+//  Created by Steven Lin on 2025-04-19.
+//
+
+import Testing
+@testable import Strava_Interview_Prep2
+
+struct Strava_Interview_Prep2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
